67a3f1c44d766ee9d5d34e6a29ea65e36e7f1ded
embrana <67813424+embrana@users.noreply.github.com>
2024-10-07 23:50:43 +0000

    cards & hamburg menu fix

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,9 +1,88 @@
 import React from "react";
 
 const Card = () => {
-	return (
-		<p>Card</p>
-	);
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-3 col-md-6 mb-4">
+                    <div className="card text-center" style={{ width: "100%" }}>
+                        <svg className="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#868e96" />
+                            <text x="50%" y="50%" fill="#dee2e6" dy=".3em" textAnchor="middle">Image cap</text>
+                        </svg>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                              when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                              It has survived not only five centuries, but also the leap into electronic</p>
+                        </div>
+                        <div className="card-footer bg-white">
+                            <a href="#" className="btn btn-primary">Find out More!</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 mb-4">
+                    <div className="card text-center" style={{ width: "100%" }}>
+                        <svg className="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#868e96" />
+                            <text x="50%" y="50%" fill="#dee2e6" dy=".3em" textAnchor="middle">Image cap</text>
+                        </svg>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                              when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                              It has survived not only five centuries, but also the leap into electronic</p>
+                        </div>
+                        <div className="card-footer bg-white">
+                            <a href="#" className="btn btn-primary">Find out More!</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 mb-4">
+                    <div className="card text-center" style={{ width: "100%" }}>
+                        <svg className="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#868e96" />
+                            <text x="50%" y="50%" fill="#dee2e6" dy=".3em" textAnchor="middle">Image cap</text>
+                        </svg>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                              when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                              It has survived not only five centuries, but also the leap into electronic</p>
+                        </div>
+                        <div className="card-footer bg-white">
+                            <a href="#" className="btn btn-primary">Find out More!</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 mb-4">
+                    <div className="card text-center" style={{ width: "100%" }}>
+                        <svg className="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#868e96" />
+                            <text x="50%" y="50%" fill="#dee2e6" dy=".3em" textAnchor="middle">Image cap</text>
+                        </svg>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                              when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                              It has survived not only five centuries, but also the leap into electronic</p>
+                        </div>
+                        <div className="card-footer bg-white">
+                            <a href="#" className="btn btn-primary">Find out More!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Card;
