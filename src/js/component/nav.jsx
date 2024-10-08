fdfec0d454bf737d6f6c509b71d34696800c16e6
embrana <67813424+embrana@users.noreply.github.com>
2024-10-08 01:19:46 +0000

    fix hamburg menu

--- src/js/component/nav.jsx
+++ src/js/component/nav.jsx
@@ -5,9 +5,15 @@ const Nav = () => {
         <nav className="navbar navbar-darkno navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-body">
             <div className="container-fluid">
                 <a className="navbar-brand text-white" href="#">Start Boostrap</a>
-                <button className="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+            <button className="navbar-toggler text-light" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav" 
+          aria-controls="navbarNav" 
+          aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
                 <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
                         <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
